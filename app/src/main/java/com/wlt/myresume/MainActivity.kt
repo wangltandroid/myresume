7b22b2d3130eae12c18f407dd22d1ff58fe5d365
wangltandroid <wanglt_android@163.com>
2021-02-24 12:05:28 +0800

    创建项目，第一次上传

---
+++ app/src/main/java/com/wlt/myresume/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.wlt.myresume
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author wangliantao
+ * @time 2021 02 2021/2/23 10:52 AM
+ * @describe：项目入口
+ * In me the tiger sniffs the rose
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
